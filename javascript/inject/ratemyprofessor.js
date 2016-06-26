1f88c4f833cfd570f0ba0aa9f9a3aa721552d92a
MustafaAbban <mustafaabban@utexas.edu>
2016-06-26 13:55:53 -0500

    RATEMYPROFESSOR

--- javascript/inject/ratemyprofessor.js
+++ javascript/inject/ratemyprofessor.js
@@ -9,3 +9,23 @@ function RMP(errorHandler) {
     //This is for teachers who have a different name on the class planner and RMP
     //When fetching data it will look if the teacher matches oldFName and oldLname and if they do
     //It will replace them with newFName and newLName
+
+
+    RATEMYPROFESSOR.prototype = Object.create(DataSection.prototype);
+    RATEMYPROFESSOR.prototype.constructor = RATEMYPROFESSOR;
+
+    /**
+     * Checks if the teacher name is in `this.teacherReplace` and if so fixes their name.
+     * @param teacher The Teacher name object. Will be modified.
+     * @private
+     */
+    RATEMYPROFESSOR.prototype.checkTeacher = function (teacher) {
+        this.teacherReplace.forEach(function (teacherCheck) {
+            if (teacher.fname === teacherCheck.oldFName && teacher.lname === teacherCheck.oldLName) {
+                teacher.fname = teacherCheck.newFName;
+                teacher.lname = teacherCheck.newLName;
+                return false; //Causes forEach to break
+            }
+            return true;
+        });
+    };
